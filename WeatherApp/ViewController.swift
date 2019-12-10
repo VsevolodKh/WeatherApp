debb8a690dfa54a5466cac314b4a3ddd3684e800
VsevolodKh <vsevolodkhodyrev@gmail.com>
2019-12-10 23:24:02 +0400

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Всеволод on 10.12.2019.
+//  Copyright © 2019 Vsevolod Khodyrev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
